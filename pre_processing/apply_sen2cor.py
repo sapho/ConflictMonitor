8a6dc58eb0e39f5d6d6f0e1d72ab6f8dfa8b3cfb
sholtkamp <S.Kokon93@gmail.com>
2019-01-11 10:51:05 +0100

    added options for resolution
    testing needed

--- pre_processing/apply_sen2cor.py
+++ pre_processing/apply_sen2cor.py
@@ -1,4 +1,4 @@
-# This script combines the steps of applying sen2cor 
+# This script combines the steps of applying sen2cor
 # for atmospheric correction and building composites
 # based on the scripts provided by Christian Knoth
 
@@ -7,28 +7,64 @@ from os import system
 import osgeo.gdal as gdal
 import os
 
-def run_correction(imgfolder):
+
+def run_correction_10(imgfolder):
     # list level 1C files in folder
     L1Cfiles = [s for s in os.listdir(imgfolder) if "L1C" in s]
-    print(str(len(L1Cfiles)) + ' level 1C file(s) found. Performing atmospheric correction.')
+    print(str(len(L1Cfiles)) +
+          ' level 1C file(s) found. Performing atmospheric correction.')
 
     # run sen2cor on level 1C files
     for file in L1Cfiles:
-        command = 'L2A_Process ' + os.path.join(imgfolder, file) + ' --resolution=10'
+        command = 'L2A_Process ' + \
+            os.path.join(imgfolder, file) + ' --resolution=10'
         system(command)
         #print('Atmospheric correction finished for ' + file)
 
     print('Done.')
 
 
-def create_composites(imgfolder, Level = '2A'):
+def run_correction_20(imgfolder):
+    # list level 1C files in folder
+    L1Cfiles = [s for s in os.listdir(imgfolder) if "L1C" in s]
+    print(str(len(L1Cfiles)) +
+          ' level 1C file(s) found. Performing atmospheric correction.')
+
+    # run sen2cor on level 1C files
+    for file in L1Cfiles:
+        command = 'L2A_Process ' + \
+            os.path.join(imgfolder, file) + ' --resolution=20'
+        system(command)
+        #print('Atmospheric correction finished for ' + file)
+
+    print('Done.')
+
+    
+def run_correction_60(imgfolder):
+    # list level 1C files in folder
+    L1Cfiles = [s for s in os.listdir(imgfolder) if "L1C" in s]
+    print(str(len(L1Cfiles)) +
+          ' level 1C file(s) found. Performing atmospheric correction.')
+
+    # run sen2cor on level 1C files
+    for file in L1Cfiles:
+        command = 'L2A_Process ' + \
+            os.path.join(imgfolder, file) + ' --resolution=60'
+        system(command)
+        #print('Atmospheric correction finished for ' + file)
+
+    print('Done.')
+
+
+def create_composites(imgfolder, Level='2A'):
 
     # create folder for composites
     try:
-        if not os.path.exists(os.path.join(imgfolder,'CIR_composites')):
-            os.makedirs(os.path.join(imgfolder,'CIR_composites'))
+        if not os.path.exists(os.path.join(imgfolder, 'CIR_composites')):
+            os.makedirs(os.path.join(imgfolder, 'CIR_composites'))
     except OSError:
-        print('Error: Creating directory', os.path.join(imgfolder,'CIR_composites'))
+        print('Error: Creating directory',
+              os.path.join(imgfolder, 'CIR_composites'))
 
     # list unzipped folders
     if Level == '2A':
@@ -38,19 +74,23 @@ def create_composites(imgfolder, Level = '2A'):
         subfolders = [s for s in os.listdir(imgfolder) if "L1C" in s]
         print("Creating 4-band CIR composites of level 1C images...")
 
-
     # in each folder find CIR bands and create composites
     for folder in subfolders:
         # navigate through GRANULE folder to band files
-        granule_folder = os.listdir(os.path.join(imgfolder, folder, 'GRANULE'))[0]
+        granule_folder = os.listdir(
+            os.path.join(imgfolder, folder, 'GRANULE'))[0]
         if Level == '2A':
-            bands_path = os.path.join(imgfolder, folder, 'GRANULE', granule_folder, 'IMG_DATA', 'R10M')
+            bands_path = os.path.join(
+                imgfolder, folder, 'GRANULE', granule_folder, 'IMG_DATA', 'R10M')
         else:
-            bands_path = os.path.join(imgfolder, folder, 'GRANULE', granule_folder, 'IMG_DATA')
+            bands_path = os.path.join(
+                imgfolder, folder, 'GRANULE', granule_folder, 'IMG_DATA')
         # create list of paths to the CIR bands, search by name to be independent from number of files in folder
         CIR_bandpaths = [os.path.join(bands_path, [s for s in os.listdir(bands_path) if "B02" in s][0]),
-                         os.path.join(bands_path, [s for s in os.listdir(bands_path) if "B03" in s][0]),
-                         os.path.join(bands_path, [s for s in os.listdir(bands_path) if "B04" in s][0]),
+                         os.path.join(bands_path, [s for s in os.listdir(
+                             bands_path) if "B03" in s][0]),
+                         os.path.join(bands_path, [s for s in os.listdir(
+                             bands_path) if "B04" in s][0]),
                          os.path.join(bands_path, [s for s in os.listdir(bands_path) if "B08" in s][0])]
 
         # create virtual layer
@@ -71,11 +111,14 @@ def create_composites(imgfolder, Level = '2A'):
         outds = gdal.Translate(outtif, outds)
         print("Composite created for " + granule_folder)
 
-    print("Finished. All composites can be found in folder " + imgfolder + '\CIR_composites.')
+    print("Finished. All composites can be found in folder " +
+          imgfolder + '\CIR_composites.')
 
 
 if __name__ == "__main__":
     imgfolder = os.environ['imgfolder']
     level = '2A'
-    run_correction(imgfolder)
+    run_correction_10(imgfolder)
+    # run_correction_20(imgfolder)
+    # run_correction_60(imgfolder)
     create_composites(imgfolder, level)
