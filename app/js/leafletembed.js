5fcfda8f00ae5fcdc8a96496ca2c3bc1aba856b2
Moritz Migge <moritz.migge@web.de>
2019-01-17 15:52:45 +0100

    Setup of landing-page 2

--- app/js/leafletembed.js
+++ app/js/leafletembed.js
@@ -4,13 +4,13 @@ map.setView([51.2, 7], 9);
 
 
 var whiteAndBlack =     //adding black and white background map
-        L.tileLayer('//{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-            subdomains: 'abcd',
-            maxZoom: 20,
-            minZoom: 0,
-            label: 'White and Black'  // optional label used for tooltip
-        }),
+    L.tileLayer('//{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        subdomains: 'abcd',
+        maxZoom: 20,
+        minZoom: 0,
+        label: 'White and Black'  // optional label used for tooltip
+    }),
     osm =           //adding osm background map
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             label: 'Street Map',
@@ -76,35 +76,10 @@ map.on(L.Draw.Event.CREATED, function (e) {
         recCoord = recCoord.replace(/{"lat":/g, '');
         recCoord = recCoord.replace(/"lng":/g, '');
         recCoord = recCoord.replace(/}/g, '');
-
+        console.log(recCoord);
     }
-    document.getElementById("coords").value = recCoord;
-
-    console.log(recCoord)
-
 });
 
 
 map.addControl(drawControl);
 
-let url_to_geotiff_file = "../layer/T46QDJ_20180104T042151_T46QDJ_20180109T042139_result_NBR.tif";
-
-fetch(url_to_geotiff_file)
-    .then(response => response.arrayBuffer())
-    .then(arrayBuffer => {
-        parseGeoraster(arrayBuffer).then(georaster => {
-            console.log("georaster:", georaster);
-            /*
-                GeoRasterLayer is an extension of GridLayer,
-                which means can use GridLayer options like opacity.
-                Just make sure to include the georaster option!
-                http://leafletjs.com/reference-1.2.0.html#gridlayer
-            */
-            var layer = new GeoRasterLayer({
-                georaster: georaster,
-                opacity: 0.7
-            });
-            layer.addTo(map);
-            map.fitBounds(layer.getBounds());
-        });
-    });
