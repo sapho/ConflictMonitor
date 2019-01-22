b9b7dc8b8c4faa33d707c73871a78f8f3cc24a9a
Sebastian Holtkamp <S.Kokon93@gmail.com>
2019-01-22 19:55:19 +0100

    Update apply_sen2cor.bat

--- pre_processing/apply_sen2cor.bat
+++ pre_processing/apply_sen2cor.bat
@@ -1,3 +1,5 @@
-docker build  -t apply . 
-docker run --rm --name=apply_container -e imgfolder=/workspace apply
+docker build -t apply . 
+docker run --name=apply_container -e imgfolder=/workspace apply
+docker cp apply_container:/workspace .
+docker rm -f apply_container
 pause
