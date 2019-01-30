f31f1fe75d78728cf5b6bc5fc8d1f13eb3c1fe07
SarahLechler <lechler.sarah@gmail.com>
2019-01-30 17:04:03 +0100

    call processing scripts

--- server/callscripts.js
+++ server/callscripts.js
@@ -1,17 +1,23 @@
 let {PythonShell} = require('python-shell');
 let R = require('r-script');
+const { exec } = require('child_process');
 
 module.exports = {
     callScripts: () => {
         R("../getSpatialData.R");
 
-        PythonShell.run('../pre_processing/apply_sen2cor.py', null, function (err) {
+        exec('../pre_processing/composites/apply_sen2cor.bat');
+        PythonShell.run('../postprocessing/NBR_BOA_Images.py', null, function (err) {
             if (err) {
                 throw err;
             }
-        })
-
+        });
+        PythonShell.run('../postprocessing/subset.py', null, function (err) {
+            if (err) {
+                throw err;
+            }
+        });
         //add other scripts here
-
+        R("../bfast.R");
     }
 };
