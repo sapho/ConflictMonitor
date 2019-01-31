616789956b63da61b6f20d2f5b1dfabbae596f16
sholtkamp <S.Kokon93@gmail.com>
2019-01-31 13:48:44 +0100

    update containers for saphirs data
    bind might be necessary

--- pre_processing/composites/apply_sen2cor.bat
+++ pre_processing/composites/apply_sen2cor.bat
@@ -1,5 +1,2 @@
 docker build --no-cache -t apply . 
-docker run --name=apply_container -e imgfolder=/workspace apply
-docker cp apply_container:/workspace .
-docker rm -f apply_container
-pause
+docker run --name=apply_container apply
